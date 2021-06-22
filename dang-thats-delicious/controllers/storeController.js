ff270b9461b7eee7438e825cbd5bb9516540cd7e
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2021-06-22 15:56:29 -0600

    added editStore.js and added a new controller

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,3 +1,7 @@
 exports.homePage = (req, res, next) => {
   res.render('index');
 }
+
+exports.addStore = (req, res, next) => {
+  res.render('editStore' { title: 'Add Store'});
+}
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
-router.get('/', storeController.homePage)   
+router.get('/', storeController.homePage)  
+router.get('/add', storeController.addStore); 
  
 module.exports = router;
