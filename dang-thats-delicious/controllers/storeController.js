936f471d3ba6d49c98b13476b73e44d96f5f55b8
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2021-06-23 15:59:32 -0600

    Added _storeForm. Added another export for submitting the form

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -1,7 +1,11 @@
 exports.homePage = (req, res, next) => {
   res.render('index');
-}
+};
 
 exports.addStore = (req, res, next) => {
-  res.render('editStore' { title: 'Add Store'});
+  res.render('editStore', { title: 'Add Store'});
+};
+
+exports.createStore = (req, res, next) => {
+  res.json(req.body);
 }
