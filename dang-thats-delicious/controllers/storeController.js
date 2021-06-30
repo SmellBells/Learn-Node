4fbacd023ea6646388f91128a16598bb90da425a
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2021-06-30 15:58:35 -0600

    Added stores.pug and _storeCard.pug. I also added to idex for routes and the controller

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -14,3 +14,16 @@ exports.createStore = async (req, res, next) => {
   req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`)
   res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores  });
+};
+
+exports.editStore = async (req, res, next) => {
+  // 1. Find the store given the I.D.
+  const store = await Store.findOne({ _id: req.params.id });
+  // 2. Confrim they are the owner of the store
+  // 3. Render out the edit form so the user can update their store
+}
--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const { catchErrors } = require('../handlers/errorHandlers');
 
-router.get('/', storeController.homePage)  
-router.get('/add', storeController.addStore); 
+router.get('/', catchErrors(storeController.getStores));
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
 router.post('/add', catchErrors(storeController.createStore));
- 
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+
 module.exports = router;
