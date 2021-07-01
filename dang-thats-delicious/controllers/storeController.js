5afa2424cd65016b96559e526248c9155b1c546c
SmellBells <elliottgonzales@elliotts-MacBook-Pro.local>
2021-07-01 16:01:10 -0600

    Added to the controller and index. Finished up editing a store so you can do so without errors

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -26,4 +26,17 @@ exports.editStore = async (req, res, next) => {
   const store = await Store.findOne({ _id: req.params.id });
   // 2. Confrim they are the owner of the store
   // 3. Render out the edit form so the user can update their store
+  res.render('editStore', { title: `Edit ${store.name}`, store });
+}
+
+exports.updateStore = async (req, res , next) => {
+  //find and update the store
+  const store = await Store.findOneAndUpdate({ _id: req.params.id }, req.body,
+    {
+    new: true, //return the new store instead of the old one
+    runValidators: true
+  }).exec();
+  req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/stores/${store.slug}">View Store ➟ </a>`);
+  res.redirect(`/stores/${store._id}/edit`);
+  // Redriect them to the store and tell them it worked
 }
